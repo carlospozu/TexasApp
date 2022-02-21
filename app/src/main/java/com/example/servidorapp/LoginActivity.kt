87b48187955ddfa28dde23f154e8766e715f566a
carlospozu <carpoz774@alu.centroafuera.es>
2022-02-21 11:57:43 +0100

    CIFRADO BIEN(tocar cosas de login)

--- app/src/main/java/com/example/servidorapp/LoginActivity.kt
+++ app/src/main/java/com/example/servidorapp/LoginActivity.kt
@@ -4,14 +4,15 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-
 import com.example.servidorapp.databinding.LoginBinding
-import com.google.gson.Gson
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import okhttp3.*
 import java.io.IOException
+import java.security.MessageDigest
+import javax.crypto.Cipher
+import javax.crypto.spec.SecretKeySpec
 
 class LoginActivity : AppCompatActivity() {
 
@@ -22,7 +23,8 @@ class LoginActivity : AppCompatActivity() {
         binding = LoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
         binding.btnLogin.setOnClickListener {
-            login(binding.etEmail.text.toString(), binding.etPassword.text.toString())
+            val passwordCifrada = cifrar(binding.password.text.toString(), generarToken())
+            login(binding.etEmail.text.toString(), passwordCifrada)
         }
 
     }
@@ -56,4 +58,33 @@ class LoginActivity : AppCompatActivity() {
             }
         })
     }
+    private fun cifrar(textoEnString : String, llaveEnString : String) : String {
+        println("Voy a cifrar: $textoEnString")
+        val cipher = Cipher.getInstance("AES/ECB/PKCS5Padding")
+        cipher.init(Cipher.ENCRYPT_MODE, getKey(llaveEnString))
+        val textCifrado = android.util.Base64.encodeToString(cipher.doFinal(textoEnString.toByteArray(Charsets.UTF_8)), android.util.Base64.URL_SAFE)
+
+        println("He obtenido $textCifrado")
+        return textCifrado
+    }
+
+    private fun getKey(llaveEnString : String): SecretKeySpec {
+        var llaveUtf8 = llaveEnString.toByteArray(Charsets.UTF_8)
+        val sha = MessageDigest.getInstance("SHA-1")
+        llaveUtf8 = sha.digest(llaveUtf8)
+        llaveUtf8 = llaveUtf8.copyOf(16)
+        return SecretKeySpec(llaveUtf8, "AES")
+    }
+
+    fun generarToken():String{
+        val alphabet = 'a'..'z'
+        var generado =""
+        for (i in 1..7){
+            val randomLetter = alphabet.random()
+            generado+=randomLetter
+        }
+        return generado
+    }
+
+
 }
