8bbbbf936b2b7829b15438488bd0037d3448de9f
carlospozu <carpoz774@alu.centroafuera.es>
2022-11-03 23:00:33 +0100

    secondo

--- app/src/main/java/com/example/servidorapp/MainActivity.kt
+++ app/src/main/java/com/example/servidorapp/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.servidorapp
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.EditText
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.servidorapp.databinding.ActivityMainBinding
@@ -17,13 +18,21 @@ import javax.crypto.spec.SecretKeySpec
 
 class MainActivity : AppCompatActivity() {
 
+
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val players =binding.numJug.text
+        val stack = binding.numFichas.text
+
         var token = intent.getStringExtra("token")
+
+        val intent = Intent(this@MainActivity, MainActivity::class.java)
+        intent.putExtra("token", body)
+        startActivity(intent)
 
     }
 }
